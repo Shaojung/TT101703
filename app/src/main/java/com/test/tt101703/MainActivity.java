245001d481a10b86ce011b09dc8513f8e71b81a4
Shaojung <shaojung@gmail.com>
2016-10-17 15:17:10 +0800

    Notification and Pending Intent

--- app/src/main/java/com/test/tt101703/MainActivity.java
+++ app/src/main/java/com/test/tt101703/MainActivity.java
@@ -2,6 +2,8 @@ package com.test.tt101703;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -21,9 +23,19 @@ public class MainActivity extends AppCompatActivity {
     public void click1(View v)
     {
         Notification.Builder builder = new Notification.Builder(MainActivity.this);
+        Intent it = new Intent(MainActivity.this, MainActivity.class);
+        it.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
+                Intent.FLAG_ACTIVITY_SINGLE_TOP |
+                Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        int requestCode = 1;
+        PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this,
+                requestCode, it, PendingIntent.FLAG_UPDATE_CURRENT);
+
         builder.setContentTitle("Test1")
                 .setContentText("This is a test")
-                .setSmallIcon(R.mipmap.ic_launcher);
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
         Notification notification = builder.build();
         nm.notify(123, notification);
     }
